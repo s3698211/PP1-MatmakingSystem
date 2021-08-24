863bf60687a861ad6a9f868fe58006b56340278e
s3763633 <s3763633@student.rmit.edu.au>
2021-08-24 14:41:45 +1000

    login/register

--- springboot-first-app/src/main/java/com/Programing_Project1_Backend/springboot/springbootfirstapp/SpringbootFirstAppApplication.java
+++ springboot-first-app/src/main/java/com/Programing_Project1_Backend/springboot/springbootfirstapp/SpringbootFirstAppApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootFirstAppApplication {
 
 	public static void main(String[] args) {
+		
+		//ridoy 
+		
 		SpringApplication.run(SpringbootFirstAppApplication.class, args);
 	}
 
